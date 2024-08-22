703a3e4764335101e8883494340ec2fa26bc7067
zhangxingyan <132345690+wowudizxy@users.noreply.github.com>
2024-08-22 19:51:00 +0800

    开发柜台间的食物转移(Develop food transfer between counters)
    1.为柜台添加一个可以指定的目标柜台变量和记录柜台生成的食材信息的变量，并在生成食材时判断，只有kitchenobject为null时才能生成。
    
    2.书写一个转移方法，有两个参数一个原本柜台，一个目标柜台，如果原本柜台为null则不能转移，如果目标柜台有东西则不能转移，然后目标柜台通过原本柜台将食材的transform设置父对象进行转移，并且setParent的第二个参数如果为true，则修改父元素的相对位置、比例和旋转，使对象保持与之前相同的世界空间位置、旋转和缩放。所以我设置false，并将原本柜台的kitchenobject赋值给目标柜台，再对原本柜台kitchenObject进行清除。

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private ClearCounter targetCounter;
+    private KitchenObject kitchenObject;
     [SerializeField] private GameObject selectCounter;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform point;
+
+    [SerializeField]private bool test = false;
+
+    private void Update ()
+    {
+        if (test && Input.GetMouseButtonDown(0))
+        {
+            TransferKitchenObject(this, targetCounter);
+        }
+    }
     public void Interact ()
     {
-        GameObject go = Instantiate(kitchenObjectSO.prefab, point, false);
-
+        if(kitchenObject == null)
+        {
+            kitchenObject = Instantiate(kitchenObjectSO.prefab, point, false).GetComponent<KitchenObject>();
+        }
     }
     public void SelectCounter ()
     {
@@ -19,5 +33,33 @@ public class ClearCounter : MonoBehaviour
     public void CancelSelect ()
     {
         selectCounter.SetActive(false);
+    }
+    private void TransferKitchenObject (ClearCounter sourceCounter,ClearCounter targetCounter)
+    {
+        if (sourceCounter.GetKitchenObject() == null)
+        {
+            Debug.LogWarning("目前柜台上没有食物");
+            return;
+        }
+        if (targetCounter.GetKitchenObject() != null)
+        {
+            Debug.LogWarning("目标柜台上有食物");
+            return;
+        }
+        targetCounter.AddKitchenObject(sourceCounter.GetKitchenObject());
+        ClearKitchenObject();
+    }
+    private void AddKitchenObject (KitchenObject kitchenObject)
+    {
+        kitchenObject.transform.SetParent(point, false);
+        this.kitchenObject = kitchenObject;
+    }
+    private KitchenObject GetKitchenObject ()
+    {
+        return kitchenObject;
+    }
+    private void ClearKitchenObject ()
+    {
+        kitchenObject = null;
     }
 }
