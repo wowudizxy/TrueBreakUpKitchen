bf1fb03e6b02740c29a7cfabcefa247c78fb7696
zhangxingyan <132345690+wowudizxy@users.noreply.github.com>
2024-08-24 20:05:27 +0800

    控制UI朝向的6种方式(Six ways to contral UI Look at)
    1.使用枚举enum进行定义几种类型的常量，并声明枚举变量mode，在update中使用switch进行检测mode
    
    2.LookAt是将UI看向摄像机，相反方向是使用B向量-a+b得到b的相反向量
    
    3.CameraForward，使UI的正方向等于摄像机的正方向。反之等于反方向
    
    4.WorldForward，使UI的正方向等于（0，0，1）反之等于（0，0，-1）

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour
 {
+    public enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraBackward,
+        WorldForward,
+        WorldBackward
+    }
+    [SerializeField] private Mode mode;
     private Image progressBar;
     private void Awake()
     {
@@ -18,7 +28,33 @@ public class ProgressBarUI : MonoBehaviour
             Debug.LogError("Progress child object not found!");
         }
     }
-
+    private void Update()
+    {
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                transform.LookAt(transform.position-Camera.main.transform.position+transform.position);
+                break;
+            case Mode.CameraForward:
+                transform.forward=Camera.main.transform.forward;
+                break;
+            case Mode.CameraBackward:
+                transform.forward =-Camera.main.transform.forward;
+                break;
+            case Mode.WorldForward:
+                transform.forward =new Vector3(0,0,1);
+                break;
+            case Mode.WorldBackward:
+                transform.forward = new Vector3(0, 0, -1);
+                break;
+            default:
+                break;
+        }
+        
+    }
     public void Show ()
     {
         gameObject.SetActive(true);
